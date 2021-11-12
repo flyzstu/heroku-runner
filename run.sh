a4a5b7ca8f82d16f16bcff4d7210b5e033d2c228
wf09 <wf.09@qq.com>
2021-11-12 20:27:33 +0800

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 mkdir actions-runner && cd actions-runner
-chmod +x simple_server
 curl -o actions-runner-linux-x64-2.284.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.284.0/actions-runner-linux-x64-2.284.0.tar.gz
 echo "1ddfd7bbd3f2b8f5684a7d88d6ecb6de3cb2281a2a359543a018cc6e177067fc  actions-runner-linux-x64-2.284.0.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-2.284.0.tar.gz
---
+++ web.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chmod +x ./simple_server
+./simple_server
