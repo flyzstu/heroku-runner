e996d970a3808281af9b27af8c7ffc212f0abc82
wf09 <wf.09@qq.com>
2021-11-12 16:13:42 +0800

    update

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ curl -o actions-runner-linux-x64-2.284.0.tar.gz -L https://github.com/actions/ru
 echo "1ddfd7bbd3f2b8f5684a7d88d6ecb6de3cb2281a2a359543a018cc6e177067fc  actions-runner-linux-x64-2.284.0.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-2.284.0.tar.gz
 
-./config.sh --url https://github.com/flyzstu/auto-generate-SSL-certificates --token AWOMWP6I3BWL2XLIMTWQPQ3BRYUWC
+./config.sh --url https://github.com/flyzstu/heroku-runner --token AWOMWP4MNGTC3XBMDBMSAO3BRYX7I
 ./run.sh
 ./sleep.sh &
 wait
